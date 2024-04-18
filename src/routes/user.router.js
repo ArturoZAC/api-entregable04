2d50ea9ffc1cc664a140951d7b75ba8e49874e08
Arturo Araujo <arturoyz2105@gmail.com>
2024-04-17 21:53:52 -0500

    addall

---
+++ src/routes/user.router.js
@@ -0,0 +1,27 @@
+const { getAll, create, getOne, remove, update, veryCode, login, logged } = require('../controllers/user.controllers');
+const express = require('express');
+const { verifyJwt } = require('../utils/verifyJWT');
+verifyJwt
+
+const routerUser = express.Router();
+
+routerUser.route('/')
+    .get(verifyJwt, getAll)
+    .post(create);
+
+routerUser.route('/login')
+    .post(login)
+
+routerUser.route('/me')
+    .get(verifyJwt, logged)
+
+
+routerUser.route('/verify/:code')
+    .get(veryCode)
+
+routerUser.route('/:id')
+    .get(verifyJwt, getOne)
+    .delete(verifyJwt, remove)
+    .put(verifyJwt, update);
+
+module.exports = routerUser;
